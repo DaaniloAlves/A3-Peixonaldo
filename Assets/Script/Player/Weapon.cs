4e7a635087eafe1ab2070fdf6bb4239d76a94f4f
Danilo <danilo77d@gmail.com>
2024-10-17 20:43:57 -0300

    salvando a scene e resolvendo erros

--- Assets/Script/Player/Weapon.cs
+++ Assets/Script/Player/Weapon.cs
@@ -4,12 +4,12 @@ public class Weapon : MonoBehaviour
 {
 	private float dano = 1f;                  // Dano da arma padrão
 	private float raioAtaque = 0.5f;
-	private LayerMask inimigoLayer;
+	// private LayerMask inimigoLayer;
 	[SerializeField] Transform posicaoAtaque;
 
 	public void Atacar(Animator animator)
 	{
-		inimigoLayer = "Inimigo";
+		// inimigoLayer = "Inimigo";
 		animator.SetTrigger("Ataque");
 		DetectarInimigos();
 	}
@@ -17,7 +17,7 @@ public class Weapon : MonoBehaviour
 	private void DetectarInimigos()
 	{
 		// Detecta os inimigos na área do ponto de ataque
-		Collider2D[] inimigos = Physics2D.OverlapCircleAll(posicaoAtaque, raioAtaque, inimigoLayer);
+		Collider2D[] inimigos = Physics2D.OverlapCircleAll(posicaoAtaque.position, raioAtaque, 1);
 
 		// Aplica dano pra cada inimigo na área
 		foreach (Collider2D inimigo in inimigos)
